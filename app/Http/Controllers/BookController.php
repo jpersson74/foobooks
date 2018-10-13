afa91883c992e42950a594685cefea2d3b33f362
jpersson <shua74@charter.net>
2018-10-12 20:42:16 -0400

    foobooks controller and route update.

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller {
+
+    /**
+     * GET /books
+     */
+    public function index()
+    {
+        return 'Here are all the books...';
+    }
+}
