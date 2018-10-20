9bab025b3fb453d6c5b2d84a526da35cd4440a14
jpersson <shua74@charter.net>
2018-10-20 09:35:50 -0400

    Added Week 7 lecture features.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -1,16 +1,20 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
-class BookController extends Controller {
-
-    /**
+class BookController extends Controller
+{
+    /*
      * GET /books
      */
     public function index()
     {
-        return 'Here are all the books...';
+        return view('books.index');
+    }
+    /*
+     * GET /books/{title}
+     */
+    public function show($title)
+    {
+        return view('books.show')->with(['title' => $title]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
-
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
+ * Home
+ */
+Route::get('/', 'WelcomeController');
+/**
+ * Books
+ */
+Route::get('/books/{title}', 'BookController@show');
 Route::get('/books', 'BookController@index');
+/**
+ * Practice
+ */
+Route::any('/practice/{n?}', 'PracticeController@index');
+# Example shown at the end of Week 6's lectures
+# to discuss the kind of planning work you can do for Project 3
+//Route::get('/', 'TriviaController@index');
+//Route::get('/check-answer', 'TriviaController@checkAnswer');
+# Example with two route parameters
+//Route::get('/books/{category}/{title}', function($category, $title) {
+//    return 'You are viewing the book: '.$title.' in the category '.$category;
+//});
